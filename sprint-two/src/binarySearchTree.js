a0cc61f8071904917579c72d696c9550f412cb15
omarzouaoui <zouaouiomar80@gmail.com>
2022-11-16 18:54:29 +0100

    binarySearchTree done

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -4,10 +4,39 @@ class BinarySearchTree {
     this.right = null;
     this.value = value;
   }
-  insert(val) {}
-  contains(val) {}
-  depthFirstLog(callBack) {}
+  insert(val) {
+    if(val>this.value){
+      if(this.right === null){
+        this.right = new BinarySearchTree(val);
+      }
+      this.right.insert(val)
+    }else if(val<this.value){
+      if(this.left === null){
+        this.left = new BinarySearchTree(val);
+      }
+      this.left.insert(val)
+    }
+  }
+  contains(val) {
+    if(this.value === val){
+      return true
+    }else if(val>this.value){
+      return !!(this.right && this.right.contains(val))
+    }else if(val<this.value){
+      return !!(this.right && this.right.contains(val))
+    }
+  }
+  
+  depthFirstLog(callBack) {
+    callBack(this.value)
+    if (this.right) {
+      this.right.depthFirstLog(callBack);
+    }if (this.left) {
+      this.left.depthFirstLog(callBack);
+    }
+  }
 }
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
