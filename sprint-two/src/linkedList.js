46535fd003e1af9f5c464e8688000e16d95077cc
omarzouaoui <zouaouiomar80@gmail.com>
2022-11-16 19:31:09 +0100

    linkedList done

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,13 +1,46 @@
 class LinkedList {
   constructor() {
     this.list = {};
+    this.head = null;
+    this.tail = null;
   }
 
-  addToTail(value) {}
+  addToTail(value) {
+  var newNode= new Node(value);
+  this.tail=newNode;
 
-  removeHead() {}
+  if(Object.keys(this.list).length===0){
+    this.head=new Node(value)
+    this.list=new Node(value)
+  }
+   else {
+    var x=this.list;
 
-  contains(target) {}
+   if(x.next!== null){
+    x=x.next;
+  }
+  x.next=this.tail;
+}
+}
+
+  removeHead() {
+    if(this.head === null){
+      return null;
+    }
+    var current = this.head;
+    this.list = this.list.next;
+    this.head = this.list;
+    return current.value;
+  }
+
+  contains(target) {
+    var node = this.list;
+    while(node){
+      if(node.value === target){
+        return true
+      }node = node.next
+    }return false
+  }
 }
 
 class Node {
@@ -16,6 +49,4 @@ class Node {
     this.next = null;
   }
 }
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+
